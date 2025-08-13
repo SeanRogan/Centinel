283b7fe1c207b2e025a2fcc56242ef7db9ae9c98
seanrogan <srogan88@gmail.com>
2025-08-12 23:20:55 -0400

    cleaning imports

--- monitorService/src/main/java/com/fedelis/centinel/monitor/model/MarketDataEvent.java
+++ monitorService/src/main/java/com/fedelis/centinel/monitor/model/MarketDataEvent.java
@@ -1,12 +1,7 @@
 package com.fedelis.centinel.monitor.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.Instant;
-import java.util.Map;
 
 @Data
 @NoArgsConstructor
