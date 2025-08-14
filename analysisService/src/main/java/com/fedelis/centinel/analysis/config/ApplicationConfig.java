fb71c56602feba308f3071555b8a96acad17a034
seanrogan <srogan88@gmail.com>
2025-08-13 23:00:48 -0400

    adding TaskManagerConfig file

--- analysisService/src/main/java/com/fedelis/centinel/analysis/config/ApplicationConfig.java
+++ analysisService/src/main/java/com/fedelis/centinel/analysis/config/ApplicationConfig.java
@@ -6,12 +6,10 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableAsync
 @EnableScheduling
 @EnableTransactionManagement
 public class ApplicationConfig {
